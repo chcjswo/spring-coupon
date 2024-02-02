afd2544c529212ad3ca36aaa37674508d14d7bf8
tony <chcjswo@gmail.com>
2024-02-03 01:27:31 +0900

    Initial commit

---
+++ src/main/java/me/mocadev/springcoupon/SpringCouponApplication.java
@@ -0,0 +1,13 @@
+package me.mocadev.springcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCouponApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/mocadev/springcoupon/SpringCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mocadev.springcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

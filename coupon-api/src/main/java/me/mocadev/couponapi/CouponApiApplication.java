556d1e9ec4a248fc537339d53d9659f6266c10c5
chcjswo <chcjswo@gmail.com>
2024-02-04 18:19:33 +0900

    feature: 서브 프로젝트 구성

--- coupon-api/src/main/java/me/mocadev/couponapi/CouponApiApplication.java
+++ coupon-api/src/main/java/me/mocadev/couponapi/CouponApiApplication.java
@@ -2,11 +2,15 @@ package me.mocadev.couponapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import me.mocadev.couponcore.CouponCoreConfiguration;
 
+@Import(CouponCoreConfiguration.class)
 @SpringBootApplication
 public class CouponApiApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "application-core, application-api");
 		SpringApplication.run(CouponApiApplication.class, args);
 	}
 
--- coupon-consumer/src/main/java/me/mocadev/couponconsumer/CouponConsumerApplication.java
+++ coupon-consumer/src/main/java/me/mocadev/couponconsumer/CouponConsumerApplication.java
@@ -2,11 +2,15 @@ package me.mocadev.couponconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import me.mocadev.couponcore.CouponCoreConfiguration;
 
+@Import(CouponCoreConfiguration.class)
 @SpringBootApplication
 public class CouponConsumerApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "application-core, application-consumer");
 		SpringApplication.run(CouponConsumerApplication.class, args);
 	}
 

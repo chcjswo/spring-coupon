aa2ef16725e52a6010298bf8e9768d1901af0d59
tony <chcjswo@gmail.com>
2024-02-10 16:26:51 +0900

    feature: redis를 사용한 동시성 제어

--- coupon-api/src/main/java/me/mocadev/couponapi/controller/CouponIssueController.java
+++ coupon-api/src/main/java/me/mocadev/couponapi/controller/CouponIssueController.java
@@ -7,16 +7,23 @@ import lombok.RequiredArgsConstructor;
 import me.mocadev.couponapi.controller.dto.CouponIssueRequestDto;
 import me.mocadev.couponapi.controller.dto.CouponIssueResponseDto;
 import me.mocadev.couponapi.service.CouponIssueRequestService;
+import me.mocadev.couponcore.component.DistributeLockExecutor;
 
 @RequiredArgsConstructor
 @RestController
 public class CouponIssueController {
 
 	private final CouponIssueRequestService couponIssueRequestService;
+	private final DistributeLockExecutor distributeLockExecutor;
 
 	@PostMapping("/v1/issue")
 	public CouponIssueResponseDto issueV1(@RequestBody CouponIssueRequestDto requestDto) {
-		couponIssueRequestService.issueRequestV1(requestDto);
+		distributeLockExecutor.execute(
+			"lock_" + requestDto.couponId(),
+			10000,
+			10000,
+			() -> couponIssueRequestService.issueRequestV1(requestDto)
+		);
 		return new CouponIssueResponseDto(true, null);
 	}
 }
